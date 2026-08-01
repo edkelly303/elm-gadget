c3438d02867b72fb125bc15988f657703c606685
Ed Kelly <edwardjkelly@gmail.com>
2026-08-01 10:15:07 +0100

    Update fleke.lock and add correct SHA for Elm 0.19.2 download

--- nix/elm/flake.nix
+++ nix/elm/flake.nix
@@ -17,7 +17,7 @@
         # https://nixos.wiki/wiki/Packaging/Binaries
         src = pkgs.fetchurl {
           url = "https://github.com/elm/compiler/releases/download/0.19.2/elm-${version}-linux-x64.gz";
-          sha256 = "";
+          sha256 = "sha256-ZjINJ3AWVPoRvQ6NhL35gpaU1XcMjc7i3t5hYPrVhzc=";
         };
         
         sourceRoot = ".";
