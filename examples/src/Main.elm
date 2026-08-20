390baa2b68722da7d022ec665b51908677fd493b
Ed Kelly <edwardjkelly@gmail.com>
2026-08-20 12:32:37 +0000

    elm-format

--- examples/src/Main.elm
+++ examples/src/Main.elm
@@ -43,7 +43,7 @@ personGadget =
             (Gadget.string |> Gadget.Adapter.Random.choose "Ed" [ "Leonardo", "Wolfgang", "Rupert", "Mario", "Martin" ])
         |> Gadget.field "heightInCentimetres"
             .heightInCentimetres
-            (Gadget.list(Gadget.float |> Gadget.Adapter.Random.range 100 180))
+            (Gadget.list (Gadget.float |> Gadget.Adapter.Random.range 100 180))
         |> Gadget.field "pets"
             .pets
             (Gadget.list petGadget |> Gadget.Adapter.Random.listLength 0 3)
