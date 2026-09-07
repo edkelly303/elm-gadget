734d414e6fe4cab6e5051e8a5cadc43a24b475ee
Ed Kelly <edwardjkelly@gmail.com>
2026-09-07 10:18:18 +0100

    Tests: get form tests working properly

--- tests/Test/FormTest.elm
+++ tests/Test/FormTest.elm
@@ -12,27 +12,25 @@ type alias Foo =
     { bar : Int, baz : String }
 
 
-g =
-    G.record Foo
-        |> G.field "bar" .bar G.int
-        |> G.field "baz"
-            .baz
-            (G.string |> F.validate (\_ -> Err "This error should show up"))
-        |> G.endRecord
-        |> F.validate (\_ -> Err "This error shouldn't show up")
-
-
-form : F.Form Foo F.Msg
-form =
-    F.fromGadget identity g
-
-
 suite : Test
 suite =
     describe "Form"
-        [ test "When a filterMapped gadget fails to submit, errors should still be returned on gadgets that are not its direct ancestors" <|
+        [ test """When a gadget fails to submit due to a parsing failure, errors should still be 
+  returned on gadgets that are not its direct ancestors""" <|
             \() ->
                 let
+                    g =
+                        G.record Foo
+                            |> G.field "bar" .bar G.int
+                            -- `bar` will fail on submission, because its initial
+                            -- stats is `""`, and that won't parse as an integer
+                            |> G.field "baz" .baz (G.string |> F.validate (\_ -> Err "This error should show up"))
+                            -- `baz` is a sibling, not a direct ancestor of `bar`
+                            |> G.endRecord
+
+                    form =
+                        F.fromGadget identity g
+
                     errors =
                         form.init
                             |> form.submit
@@ -44,9 +42,25 @@ suite =
                             , { error = "This error should show up", path = [ "baz" ] }
                             ]
                         )
-        , test "When a filterMapped gadget fails to submit, errors on its direct ancestors should not be returned" <|
+        , test """When a gadget fails to submit due to a parsing failure, errors on its direct 
+  ancestors should not be returned""" <|
             \() ->
                 let
+                    g =
+                        G.record Foo
+                            |> G.field "bar" .bar G.int
+                            -- `bar` will fail on submission, because its initial
+                            -- stats is `""`, and that won't parse as an integer
+                            |> G.field "baz"
+                                .baz
+                                G.string
+                            |> G.endRecord
+                            |> F.validate (\_ -> Err "This error shouldn't show up")
+                            -- this error is on the root gadget, which is a direct ancestor of `bar`
+
+                    form =
+                        F.fromGadget identity g
+
                     errors =
                         form.init
                             |> form.submit
@@ -55,7 +69,6 @@ suite =
                     |> Expect.equal
                         (Err
                             [ { error = "Not an integer", path = [ "bar" ] }
-                            , { error = "This error should show up", path = [ "baz" ] }
                             ]
                         )
         ]
